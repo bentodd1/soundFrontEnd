fd3d8176e7dc06b76391d4d0dc317b2303904f77
Ben Todd <btodd@multiservice.com>
2018-12-12 18:11:26 -0600

    Got menu items to be dynamic  added menu item

---
+++ src/data/recordings.js
@@ -0,0 +1,10 @@
+const recordings = {
+    all: [{
+        id: 0,
+        name: 'cosmicVoyageDreaming',
+        text: 'Explore space',
+        //src: images('./cosmicVoyageDreaming.jpg'),
+      }],
+
+};
+export default recordings;
---
+++ src/pages/Menu.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page class="flex flex-center">
+      <q-btn color="primary" class="full-width">
+       <label>
+        {{scenes.all[0].name}}
+          </label>
+        </q-btn>
+      <q-btn color="primary" class="full-width" label="Full-width" />
+     <!--<div>Choose your dream</div> -->
+       </q-page>
+</template>
+
+<script>
+import scenes from '../data/recordings';
+
+export default {
+      name: 'PageMenu',
+    data(){
+        return {
+        scenes,
+        };
+    },
+
+
+};
+
+</script>
